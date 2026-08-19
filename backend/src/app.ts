aaa166da6c7d12a7311f603849eeb365a6af262e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-19 16:14:08 -0700

    feat: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 import postRoutes from "./modules/posts/postRoutes.js";
 const app = express();
 
+app.use(cors({
+    origin:"http://localhost:3000"
+}));
 app.use(express.json());
-app.use("feed",postRoutes);
+app.use("/feed",postRoutes);
+
 export {app};
