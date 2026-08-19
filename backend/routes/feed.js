ecd8ac9b32f339303c764d3a9c3c1488de481e64
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-19 13:49:20 -0700

    chroe: init backend with new stack

--- backend/routes/feed.js
+++
@@ -1,8 +0,0 @@
-import e from "express"
-import { getPosts, postPost } from "../controllers/feed.js";
-const router = e.Router();
-
-router.get("/posts",getPosts);
-router.post("/post",postPost);
-
-export{router};
