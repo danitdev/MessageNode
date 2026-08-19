42e454e9b3c66dc93eaff890253726e9cd13fbe7
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-19 15:18:02 -0700

    feat: add post routes to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import postRoutes from "./modules/posts/postRoutes.js";
 const app = express();
 
 app.use(express.json());
-
+app.use("feed",postRoutes);
 export {app};
