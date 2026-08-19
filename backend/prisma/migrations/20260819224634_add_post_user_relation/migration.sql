ad6d43e9c191b946db802bbc5d5bd9aa1cc519b2
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-19 16:12:58 -0700

    feat: add migration for adding user and post relations

---
+++ backend/prisma/migrations/20260819224634_add_post_user_relation/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
