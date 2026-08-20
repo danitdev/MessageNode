61dce4d41a3c1f29d8adb97a83332425be722b1e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-20 11:30:44 -0700

    feat: add validation for create post

--- backend/src/modules/posts/postRoutes.ts
+++ backend/src/modules/posts/postRoutes.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import {getPosts} from "./postController.js";
+import {getPosts,postPost} from "./postController.js";
+import {createPostSchema} from "./postSchema.js";
+import { validate } from "../../middleware/validate.js";
 const router = Router();
 
 router.get("/posts",getPosts);
-
+router.post("/post",validate(createPostSchema),postPost)
 
 
 export default router;
