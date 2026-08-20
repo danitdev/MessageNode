71db67b8b4ec1b2088d81c6d3e235b1197e6ce3e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-20 11:31:05 -0700

    feat: add POST post

--- backend/src/modules/posts/postController.ts
+++ backend/src/modules/posts/postController.ts
@@ -1,6 +1,7 @@
 
 import type { Request,Response,NextFunction } from "express";
-import {getPostsService} from "./postService.js";
+import {getPostsService,postPostService} from "./postService.js";
+import {CreatePostInput} from "./postSchema.js"
 export const getPosts = async (
     req:Request,
     res:Response,
@@ -13,3 +14,19 @@ export const getPosts = async (
         next(err);
     }
 }
+export const postPost = async (
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+        try{
+            const data:CreatePostInput = {
+                title:req.body.title,
+                content:req.body.content
+            }
+            const post = await postPostService(data);
+            res.status(201).json({post});
+        }
+        catch(err){
+            next(err);
+        }
+    }
