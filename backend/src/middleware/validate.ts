be6f28e7b49afe96a146ebf5354e6ac63f54f400
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-22 11:23:28 -0700

    refactor: status on validation changed

--- backend/src/middleware/validate.ts
+++ backend/src/middleware/validate.ts
@@ -4,8 +4,9 @@ import {ZodType} from "zod";
 export const validate = (schema:ZodType)=>{
     return (req:Request, res:Response,next:NextFunction) => {
         const result = schema.safeParse(req.body);
+        console.log(result.error?.flatten());
         if(!result.success){
-            return res.status(400).json({
+            return res.status(422).json({
                 message:"Validation failed",
                 errors:result.error.flatten()
             });
