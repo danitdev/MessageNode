e75bbfd12447fd3389b6a59ea4b7e6b1c9bc2f1a
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-22 11:23:45 -0700

    refactor: change the value on min title and min content

--- backend/src/modules/posts/postSchema.ts
+++ backend/src/modules/posts/postSchema.ts
@@ -4,12 +4,12 @@ export const createPostSchema = z.object({
     title:z
         .string()
         .trim()
-        .min(1,"Title is required!")
+        .min(5,"Title should be a min of 5 char!")
         .max(100,"Title must be at most 100 characters"),
     content:z
         .string()
         .trim()
-        .min(1,"Content is required!")
+        .min(5,"Content should be a min of 5 char!")
         .max(5000,"Content must be at most 5000 characters.")
 });
 export type CreatePostInput = z.infer<typeof createPostSchema>;
