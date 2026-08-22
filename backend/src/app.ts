294a7a19e84cd22b78e5223f05a885c2ab21ce6e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-22 11:24:06 -0700

    feat: add file storage using disk storage

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,22 @@
 import express from "express";
 import cors from "cors";
+import multer, { Multer } from "multer";
 import postRoutes from "./modules/posts/postRoutes.js";
+
+
+const fileStorage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images");
+
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+"-"+file.originalname);
+    }
+});
+
+
+
+const upload = multer({storage:multer.memoryStorage()});
 const app = express();
 
 app.use(cors({
