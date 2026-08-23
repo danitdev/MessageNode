4731ee9504efbbfae6e6ea1f78add954dfd45692
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-23 14:22:13 -0700

    feat: using app error class for validation error handling

--- backend/src/middleware/validate.ts
+++ backend/src/middleware/validate.ts
@@ -1,15 +1,20 @@
 import {Request,Response,NextFunction} from "express";
 import {ZodType} from "zod";
+import {AppError} from "../errors/AppError.js"
 
 export const validate = (schema:ZodType)=>{
     return (req:Request, res:Response,next:NextFunction) => {
         const result = schema.safeParse(req.body);
         console.log(result.error?.flatten());
         if(!result.success){
-            return res.status(422).json({
-                message:"Validation failed",
-                errors:result.error.flatten()
-            });
+            const error = new AppError(
+                "Validation failed, entered data is incorrect.",
+                422);
+            throw error;
+            // return res.status(422).json({
+            //     message:"Validation failed",
+            //     errors:result.error.flatten()
+            // });
         }
         req.body = result.data;
         next();
