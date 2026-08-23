37c4d3dd089f51cd755dcd75d251f0a7b801a6d7
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-23 14:54:45 -0700

    feat: add get post to post controller

--- backend/src/modules/posts/postController.ts
+++ backend/src/modules/posts/postController.ts
@@ -1,9 +1,8 @@
 
 import type { Request,Response,NextFunction } from "express";
-import {getPostsService,postPostService} from "./postService.js";
+import {getPostsService,getPostService,postPostService} from "./postService.js";
 import {CreatePostInput} from "./postSchema.js"
 import {AppError} from "../../errors/AppError.js"
-import { errorMonitor } from "events";
 
 export const getPosts = async (
     req:Request,
@@ -17,6 +16,28 @@ export const getPosts = async (
         next(err);
     }
 }
+export const getPost = async(
+    req:Request,
+    res:Response,
+    next:NextFunction)=>{
+    try{
+        const postId = Number(req.params.postId);
+        const post = await getPostService(postId);
+        console.log("cool");
+        if(!post){
+            throw new AppError("Post not found!",404);
+        }
+        res.status(200).json({message:"Post fetched.",post:post});
+    }
+    catch(err){
+        if(err instanceof AppError){
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        }
+    }
+};
 export const postPost = async (
     req:Request,
     res:Response,
