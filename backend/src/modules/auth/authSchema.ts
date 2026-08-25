1b66c2563aaefb7545fa5e7f0b3b6b70204a3e78
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-25 13:51:34 -0700

    feat: add validation for registering a user

---
+++ backend/src/modules/auth/authSchema.ts
@@ -0,0 +1,17 @@
+import {z} from "zod";
+
+export const registerUserSchema = z.object({
+    name:z
+        .string()
+        .trim()
+        .min(5,"Name should be a min of 5 char!")
+        .max(40,"Name must be at most 40 characters."),
+    email:z
+        .email("Invalid Email Address!")
+    ,
+    password:z
+        .string()
+        .min(8,"password must be at least 8 characters.")
+});
+
+export type RegisterUserInput = z.infer<typeof registerUserSchema>;
