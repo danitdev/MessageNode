badf3f3deeea3de523f24df9c08e8b391820a72a
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-24 17:12:59 -0700

    feat: add update post route

--- backend/src/modules/posts/postRoutes.ts
+++ backend/src/modules/posts/postRoutes.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {getPosts,getPost,postPost} from "./postController.js";
+import {getPosts,getPost,postPost,updatePost} from "./postController.js";
 import {createPostSchema} from "./postSchema.js";
 import { validate } from "../../middleware/validate.js";
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.get("/posts",getPosts);
 router.post("/post",validate(createPostSchema),postPost)
 router.get("/post/:postId",getPost);
+router.put("/post/:postId",validate(createPostSchema),updatePost);
 
 export default router;
