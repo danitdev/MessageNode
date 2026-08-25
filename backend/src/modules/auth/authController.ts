ce685c00fb3e6902b1b146587e5614955be22f21
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-25 14:56:45 -0700

    feat: use registerUserService in the signUp controller

--- backend/src/modules/auth/authController.ts
+++ backend/src/modules/auth/authController.ts
@@ -1,5 +1,5 @@
 import type {Request,Response,NextFunction} from "express";
-import {} from "./authSevice.js";
+import {registerUserService} from "./authSevice.js";
 import {} from "./authSchema.js";
 import {AppError} from "../../errors/AppError.js";
 
@@ -12,6 +12,8 @@ export const signUp = async (
             const email = req.body.email;
             const name = req.body.name;
             const password = req.body.password;
+            const user = await registerUserService(name,email,password);
+            res.status(201).json({message:"User Created!",userId:user.id});
         }catch(err){
             if(err instanceof AppError){
                 if(!err.statusCode){
