e990c8338cd5ad030c69f64f1a7fa02312a926b3
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-25 14:57:19 -0700

    feat: hash the pass and creating new user and returning it

--- backend/src/modules/auth/authSevice.ts
+++ backend/src/modules/auth/authSevice.ts
@@ -1,12 +1,38 @@
 import {prisma} from "../../lib/prisma.js";
 import {} from "./authSchema.js";
 import {AppError} from "../../errors/AppError.js";
-
+import argon2 from "argon2"
+import { hasSubscribers } from "diagnostics_channel";
 
 export const registerUserService = async(name:string,email:string,password:string)=>{
     const user = await prisma.user.findUnique({where:{email:email}});
+    let hashedPassword:string;
     if(user){
-        throw new AppError("E-Mail address already exists!",503);
+        throw new AppError("E-Mail address already exists!",409);
+    }
+    try{
+        hashedPassword = await argon2.hash(password,{
+            type: argon2.argon2id,
+            memoryCost:65536,
+            timeCost:3,
+            parallelism:4
+        });
+    }
+    catch(err){
+        console.log(err);
+        throw new AppError("Failed to hash password",500);
+    }
+    try{
+        const newUser = await prisma.user.create({data:{
+            email:email,
+            name:name,
+            password:hashedPassword
+        }});
+        return newUser;
+    }
+    catch(err){
+        console.log(err);
+        throw new AppError("Failed to create user",500);
     }
 
 }
