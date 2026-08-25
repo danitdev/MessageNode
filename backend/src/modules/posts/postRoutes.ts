a4a17b38e5b724900ee8636b905a15c5e8241fa3
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-24 18:15:03 -0700

    feat:add delete post route

--- backend/src/modules/posts/postRoutes.ts
+++ backend/src/modules/posts/postRoutes.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {getPosts,getPost,postPost,updatePost} from "./postController.js";
+import {getPosts,getPost,postPost,updatePost,deletePost} from "./postController.js";
 import {createPostSchema} from "./postSchema.js";
 import { validate } from "../../middleware/validate.js";
 const router = Router();
@@ -8,5 +8,6 @@ router.get("/posts",getPosts);
 router.post("/post",validate(createPostSchema),postPost)
 router.get("/post/:postId",getPost);
 router.put("/post/:postId",validate(createPostSchema),updatePost);
+router.delete("/post/:postId",deletePost);
 
 export default router;
