42c2eace389225659b6d75f7de240f914a95a3ce
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-25 14:08:24 -0700

    feat: add singUp controller without the logic yet only try and catch block with setting the statusCode of error

--- backend/src/modules/auth/authController.ts
+++ backend/src/modules/auth/authController.ts
@@ -2,3 +2,22 @@ import type {Request,Response,NextFunction} from "express";
 import {} from "./authSevice.js";
 import {} from "./authSchema.js";
 import {AppError} from "../../errors/AppError.js";
+
+
+export const signUp = async (
+    req:Request,
+    res:Response,
+    next:NextFunction) =>{
+        try{
+            const email = req.body.email;
+            const name = req.body.name;
+            const password = req.body.password;
+        }catch(err){
+            if(err instanceof AppError){
+                if(!err.statusCode){
+                    err.statusCode = 500;
+                }
+            }
+            next(err);
+        }
+};
