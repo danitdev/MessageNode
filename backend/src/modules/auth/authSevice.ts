fac100a68d5bca5a5effb6ff6b62156bbaf46ef5
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-25 14:06:16 -0700

    feat: check for the user email cuz email is unique

--- backend/src/modules/auth/authSevice.ts
+++ backend/src/modules/auth/authSevice.ts
@@ -1,3 +1,12 @@
 import {prisma} from "../../lib/prisma.js";
 import {} from "./authSchema.js";
 import {AppError} from "../../errors/AppError.js";
+
+
+export const registerUserService = async(name:string,email:string,password:string)=>{
+    const user = await prisma.user.findUnique({where:{email:email}});
+    if(user){
+        throw new AppError("E-Mail address already exists!",503);
+    }
+
+}
