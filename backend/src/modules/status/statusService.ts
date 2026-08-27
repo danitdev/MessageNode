cac393e79e556fd5c80179a1537ffc044fd0b28e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-27 13:38:44 -0700

    feat: add patch status service

--- backend/src/modules/status/statusService.ts
+++ backend/src/modules/status/statusService.ts
@@ -9,3 +9,20 @@ export const getStatusService = async(userId:number)=>{
     }
     return user.status;
 }
+
+export const patchStatusService = async(userId:number,updatedStatus:string)=>{
+    const user = await prisma.user.findUnique({where:{id:userId},select:{status:true,id:true}})
+    if(!user){
+        throw new AppError("User not found!",404);
+    }
+    const updatedUser = await prisma.user.update(
+        {
+            where:{
+                id:userId
+            },
+            data:{
+                status:updatedStatus
+            }
+        });
+    return true;
+}
