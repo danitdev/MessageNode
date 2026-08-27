150cad6f3e2126eb82dd645df68f2cb30435be4e
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-27 13:38:18 -0700

    fix: using patchStatus route

--- backend/src/modules/status/statusRoutes.ts
+++ backend/src/modules/status/statusRoutes.ts
@@ -1,10 +1,10 @@
 import {Router} from "express";
-import {getStatus,postStatus} from "./statusController.js";
-
+import {getStatus,patchStatus} from "./statusController.js";
+import {isAuth} from "../../middleware/isAuth.js";
 const statusRouter = Router();
 
-statusRouter.get("/status",getStatus);
-statusRouter.post("/status",postStatus);
+statusRouter.get("/status",isAuth,getStatus);
+statusRouter.patch("/status",isAuth,patchStatus);
 
 
 export default statusRouter;
