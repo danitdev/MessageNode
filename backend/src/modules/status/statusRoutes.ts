dae339d0afe537245be0d2d2602be7acbf0e1269
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-27 13:44:07 -0700

    feat: add validation middleware to status

--- backend/src/modules/status/statusRoutes.ts
+++ backend/src/modules/status/statusRoutes.ts
@@ -1,10 +1,12 @@
 import {Router} from "express";
 import {getStatus,patchStatus} from "./statusController.js";
 import {isAuth} from "../../middleware/isAuth.js";
+import {checkStatusSchema} from "./statusSchema.js";
+import {validate} from "../../middleware/validate.js";
 const statusRouter = Router();
 
 statusRouter.get("/status",isAuth,getStatus);
-statusRouter.patch("/status",isAuth,patchStatus);
+statusRouter.patch("/status",isAuth,validate(checkStatusSchema),patchStatus);
 
 
 export default statusRouter;
