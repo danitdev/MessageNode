5ef38f5dc92371de20ba34b7fd83ac81897e46f8
Danibignarty <94107905+Danit8@users.noreply.github.com>
2026-08-26 17:45:41 -0700

    feat: add isAuth middleware to getPosts

--- backend/src/modules/posts/postRoutes.ts
+++ backend/src/modules/posts/postRoutes.ts
@@ -2,9 +2,12 @@ import {Router} from "express";
 import {getPosts,getPost,postPost,updatePost,deletePost} from "./postController.js";
 import {createPostSchema} from "./postSchema.js";
 import { validate } from "../../middleware/validate.js";
+import {isAuth} from "../../middleware/isAuth.js";
+
+
 const router = Router();
 
-router.get("/posts",getPosts);
+router.get("/posts",isAuth,getPosts);
 router.post("/post",validate(createPostSchema),postPost)
 router.get("/post/:postId",getPost);
 router.put("/post/:postId",validate(createPostSchema),updatePost);
